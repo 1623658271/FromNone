08ec50dbcc7d132f0be929fb33473800dde26d52
1623658271 <1623658271@qq.com>
2022-08-12 17:48:32 +0800

    无

--- LfyRepositoryX/src/main/java/com/fromnone/lfyrepositoryx/utils/ActivityUtils.kt
+++ LfyRepositoryX/src/main/java/com/fromnone/lfyrepositoryx/utils/ActivityUtils.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * description ： 活动工具类
+ * description ： Activity工具类
  * author : lfy
  * email : 1623658271@qq.com
  * date : 2022/8/11 15:42
